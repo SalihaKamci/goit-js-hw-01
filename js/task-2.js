1fa316c6fd30dfbe699648c71370e5ea6567625a
SalihaKamci <salihakamci06@gmail.com>
2025-05-13 22:43:49 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function getShippingMessage(country,price,deliveryFee){
- const message=`"Shipping to ${country} will cost ${price+deliveryFee} credits"`
- return console.log(message);
+ const message=`Shipping to ${country} will cost ${price+deliveryFee} credits`
+ return (message);
 }
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
