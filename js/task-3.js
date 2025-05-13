10b3c60cb28b5d55b76baab659654bcac775248c
SalihaKamci <salihakamci06@gmail.com>
2025-05-13 22:44:01 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth (content,padding,border ){
     const message= Number.parseFloat(content)+(2*Number.parseFloat(padding))+(2*Number.parseFloat(border));
-    return console.log(message);
+    return (message);
 
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
 
